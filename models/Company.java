95704b3ff8727454064ae7839ce3720af9554001
sumaiyahasan18 <sumaiyahasan.s.h.m@gmail.com>
2023-09-30 00:48:33 +0600

    added comments

--- models/Company.java
+++ models/Company.java
@@ -9,6 +9,7 @@ public class Company {
     public Company(int name, int id, String location) {
         this.name = name;
         this.id = id;
-        this.location =location;
+        this.location = location;
     }
+    // hello world
 }
