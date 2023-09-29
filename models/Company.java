91c4838f2f21a297d3bbf12f1a723576d22e8f3e
sumaiyahasan18 <sumaiyahasan.s.h.m@gmail.com>
2023-09-30 03:40:22 +0600

    add class

--- models/Company.java
+++ models/Company.java
@@ -1,15 +1,37 @@
 package models;
 
+import java.util.ArrayList;
+
 public class Company {
 
-    private int name;
+    private String name;
     private int id;
     private String location;
+    public static int count = 0;
+    public static ArrayList<Company> companies = new ArrayList<Company>();
 
-    public Company(int name, int id, String location) {
+    public Company(String name, String location) {
         this.name = name;
-        this.id = id;
+        this.id = Company.count + 1;
         this.location = location;
+        Company.companies.add(this);
     }
-    // hello world
+
+    public static Company create(String name, String location) {
+        Company company = new Company(name, location);
+        return company;
+    }
+
+    public String toString() {
+        return this.name + " " + this.location + " " + this.id;
+    }
+
+    public static Company searchById(int _id) {
+        for (int i = 0; i < Company.companies.size(); i++) {
+            if (Company.companies.get(i).id == _id) {
+                return Company.companies.get(i);
+            }
+        }
+        return null;
+    }
 }
---
+++ models/Index.java
@@ -0,0 +1,17 @@
+package models;
+
+import models.Company;
+import models.Inventory;
+import models.Invoice;
+import models.Product;
+import models.Purchase_log;
+import models.Sales_log;
+import models.Stock;
+import models.CustomerInfo;
+
+public class Index {
+    public static void main(String[] args) {
+
+    }
+
+}
